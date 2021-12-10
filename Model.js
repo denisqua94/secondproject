aae0f4b8b890748b8957cf59cf2d0c00fa60c5d4
Mansur Makhmutov <sir.makhmutov@gmail.com>
2021-12-10 15:00:04 +0300

    model1

--- Model.js
+++ Model.js
@@ -3,6 +3,16 @@ const readline = require("readline");
 
 class Model {
     constructor() {}
+
+    async getMenu() {
+        const menu = await fs.readdir("./topics");
+        // console.log(menu);
+        for (let i = 0; i < menu.length; i += 1) {
+            console.log(`${i + 1}. ${menu[i].slice(0, -4)}`);
+        }
+    }
 }
 
+const model = new Model();
+console.log(model.getMenu());
 module.exports = Model;
